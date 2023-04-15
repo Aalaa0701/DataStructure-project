236e5d9bcbfba8d1a9d735d35ef6fd91347bdfd5
Aalaa0701 <Alaaalaaashour1507@gmail.com>
2023-04-15 23:37:56 +0200

    user classAdded

--- DSproject/user.h
+++ DSproject/user.h
@@ -3,10 +3,62 @@
 #include <vector>
 #include <queue>
 #include <algorithm>
+using namespace std;
 
 class User
 {
-   
+    string name;
+    string national_id;
+    string password;
+    char gender;
+    int age;
+    string governorate;
+    bool vaccinated;
+    bool received_both_doses;
+
+
+public:
+    // Constructor
+    User(string name, string national_id, string password, char gender, int age,
+        string governorate, bool vaccinated = false, bool received_both_doses = false) :
+        name(name), national_id(national_id), password(password), gender(gender), age(age),
+        governorate(governorate), vaccinated(vaccinated), received_both_doses(received_both_doses) {}
+
+    // Getters
+    string get_name() const {
+        return name;
+    }
+    string get_national_id() const {
+        return national_id;
+    }
+    string get_password() const {
+        return password;
+    }
+    char get_gender() const {
+        return gender;
+    }
+    int get_age() const {
+        return age;
+    }
+    string get_governorate() const {
+        return governorate;
+    }
+    bool is_vaccinated() const {
+        return vaccinated;
+    }
+    bool has_received_both_doses() const {
+        return received_both_doses;
+    }
+
+    // Setters
+    void set_vaccinated(bool vaccinated) {
+        this->vaccinated = vaccinated;
+    }
+    void set_received_both_doses(bool received_both_doses) {
+        this->received_both_doses = received_both_doses;
+    }
+
+
 
 };
 
