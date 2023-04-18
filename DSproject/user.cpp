14a6308dbe485345fc055424eab2d41b7b6a1d53
sosybosyy <esraa.ali12361@gmail.com>
2023-04-18 09:27:57 +0200

    deletingIterators

--- DSproject/user.cpp
+++ DSproject/user.cpp
@@ -17,37 +17,33 @@ void Admin::Statistics(vector<User>firstDose,vector<User>secondDose,queue<User>w
 	int firstRange1 = 0; int secondRange1 = 0; int thirdRange1 = 0; int fourthRange1 = 0; int fifthRange1 = 0;
 	int firstRange2 = 0; int secondRange2 = 0; int thirdRange2 = 0; int fourthRange2 = 0; int fifthRange2 = 0;
 	int firstRange = 0; int secondRange = 0; int thirdRange = 0; int fourthRange = 0; int fifthRange = 0;
-	vector<User>::iterator it;
+	/*vector<User>::iterator it;
 	vector<User>::iterator it1;
 	vector<User>::iterator it2;
-	vector<User>::iterator it3;
+	vector<User>::iterator it3;*/
 
 	//calculate the pct of first and second dose
 	firstDosePct = (firstDose.size() / (firstDose.size() + secondDose.size() + waitingList.size()))*100.00;
 	secondDosePct = (secondDose.size()/ (firstDose.size() + secondDose.size() + waitingList.size()))*100.00;
 
 	//filter males and females from first dose vector
-	it = firstDose.begin();
-	it1 = secondDose.begin();
+	/*it = firstDose.begin();
+	it1 = secondDose.begin();*/
 	for (int i= 0; i < firstDose.size(); i++) {
 		if (firstDose[i].get_gender() == 'M') {
 			mCount++;
-			it++;
 		}
 		else if (firstDose[i].get_gender() == 'F') {
 			fCount++;
-			it++;
 		}
 	}
 	//filter males and females from second dose vector
 	for (int i = 0; i < secondDose.size(); i++) {
 		if (secondDose[i].get_gender() == 'M') {
 			mCount++;
-			it1++;
 		}
 		else if (secondDose[i].get_gender() == 'F') {
 			fCount++;
-			it1++;
 		}
 	}
 	// filter males and females from waiting list
@@ -69,51 +65,39 @@ void Admin::Statistics(vector<User>firstDose,vector<User>secondDose,queue<User>w
 
 
 	//filter each age group from second dose vector
-	it2 = secondDose.begin();
-	it3 = firstDose.begin();
 	for (int i = 0; i < secondDose.size(); i++) {
 		if (secondDose[i].get_age() >= 75) {
 			firstRange++;
-			it2++;
 		}
 		else if (secondDose[i].get_age() >= 65 && secondDose[i].get_age() <= 74) {
 			secondRange++;
-			it2++;
 		}
 		else if (secondDose[i].get_age() >= 50 && secondDose[i].get_age() <= 64) {
 			thirdRange++;
-			it2++;
 		}
 		else if (secondDose[i].get_age() >= 40 && secondDose[i].get_age() <= 49) {
 			fourthRange++;
-			it2++;
 		}
 		else if (secondDose[i].get_age() >= 18 && secondDose[i].get_age() <= 39) {
 			fifthRange++;
-			it2++;
 		}
 	}
 	//filter each age group from first dose vector
 	for (int i = 0; i < firstDose.size(); i++) {
 		if (firstDose[i].get_age() >= 75) {
 			firstRange1++;
-			it3++;
 		}
 		else if (firstDose[i].get_age() >= 65 && firstDose[i].get_age() <= 74) {
 			secondRange1++;
-			it3++;
 		}
 		else if (firstDose[i].get_age() >= 50 && firstDose[i].get_age() <= 64) {
 			thirdRange1++;
-			it3++;
 		}
 		else if (firstDose[i].get_age() >= 40 && firstDose[i].get_age() <= 49) {
 			fourthRange1++;
-			it3++;
 		}
 		else if (firstDose[i].get_age() >= 18 && firstDose[i].get_age() <= 39) {
 			fifthRange1++;
-			it3++;
 		}
 	}
 	//filter each age group from waiting list
